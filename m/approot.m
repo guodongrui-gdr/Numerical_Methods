f54cfc6dfb517cda445f9f073e2d5791edd6a641
makabaka003 <3084608158@qq.com>
2022-05-15 04:08:05 +0800

    approot

--- m/approot.m
+++ m/approot.m
@@ -3,7 +3,7 @@ function R=approot(f,a,b,epsilon)
 %           X为向量形式的自变量
 %           epsilon为最大误差
 % 输出参数: R为根的近似值位置
-X=a:10^(-6):b;
+X=a:10^(-5):b;
 Y=f(X);
 df=diff(f(X));
 n=length(X);
@@ -32,7 +32,4 @@ for k=1:n-1
             m=m-1;
         end
     end
-end
-if isempty(R)
-    disp("区间内没有根")
 end
