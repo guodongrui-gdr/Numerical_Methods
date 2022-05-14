f34b9a2a69a7ce608d29e333073546cc1ae9f3b9
makabaka003 <3084608158@qq.com>
2022-05-14 17:09:42 +0800

    牛顿法

--- m/newton.m
+++ m/newton.m
@@ -12,13 +12,9 @@ function [p,k,err,y]=newton(f,p0,delta,epsilon,max1,M)% M重根情况下的牛顿迭代法
 %           y为f(p)
 syms x;
 g=f(x);
-df=matlabFunction(diff(g,1));
+df=diff(f,x);
 for k=1:max1
-    if feval(df,p0)==0
-        disp(p0);
-        break;
-    end
-    p1=p0-M*feval(f,p0)/feval(df,p0);
+    p1=p0-M*feval(f,p0)/double(subs(df,x,p0));
     err(k)=abs(p0-p1);
     relerr=2*err/(abs(p1)+delta);%相对误差
     p(k)=p1;
