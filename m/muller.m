0bb59d14f45152fa3d80ade9db50157be2db8112
makabaka003 <3084608158@qq.com>
2022-05-14 13:01:16 +0800

    米勒法

--- m/muller.m
+++ m/muller.m
@@ -8,6 +8,10 @@ function [p,k,y]=muller(f,p0,p1,p2,delta,epsilon,max1)
 %           k为迭代次数
 %           y为f(p)
 P=[p0 p1 p2];
+[~,i]=min(abs(f(P)));
+tmp=P(i);
+P(i)=P(3);
+P(3)=tmp;
 for k=1:max1
     F=feval(f,P);
     h0=P(1)-P(3);h1=P(2)-P(3);
@@ -19,6 +23,9 @@ for k=1:max1
         z=(-2*c)/(b+sqrt(b^2-4*a*c));
     elseif(b<0)
         z=(-2*c)/(b-sqrt(b^2-4*a*c));
+    end
+    if ~isreal(z)
+        z=real(z);
     end
     p(k)=P(3)+z;
     [~,i]=max(abs(P-p(k)));
