70c055c23b1e0d12d903c2c12dcb75c9e5b7fb7c
makabaka003 <3084608158@qq.com>
2022-07-04 13:39:57 +0800

    龙贝格提阶公式

---
+++ test2/romber.m
@@ -0,0 +1,23 @@
+function [R,quad,err,h]=romber(f,a,b,n,tol)
+M=1;
+h=b-a;
+err=1;
+J=0;
+R=zeros(4,4);
+R(1,1)=h*(feval(f,a)+feval(f,b))/2;
+while((err>tol)&(J<n))|(J<4)
+    J=J+1;
+    h=h/2;
+    s=0;
+    for p=1:M
+        x=a+h*(2*p-1);
+        s=s+feval(f,x);
+    end
+    R(J+1,1)=R(J,1)/2+h*s;
+    M=2*M;
+    for K=1:J
+        R(J+1,K+1)=R(J+1,K)+(R(J+1,K)-R(J,K))/(4^K-1);
+    end
+    err=abs(R(J,J)-R(J+1,K+1));
+end
+quad=R(J+1,J+1);
